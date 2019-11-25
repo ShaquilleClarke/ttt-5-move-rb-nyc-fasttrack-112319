e09cd78aca9224c3e69d0c019bf552d1d1582d17
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-11-25 20:46:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,5 +9,5 @@ end
 # code your input_to_index and move method here!
 user_input = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 def input_to_index(user_input)
-  puts
+  puts "#{user_input[4.to_i]}"
 end  
